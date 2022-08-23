d2c34c178ebc25bb95684cd69bf378a5886443ef
Andsonb <andersonboy.XD@gmail.com>
2022-08-23 15:31:58 -0300

    aula

---
+++ aula/aula14/aula14.py
@@ -0,0 +1,7 @@
+nome='anderson barbosa da silva bernardo'
+
+nomelen=len(nome)
+contador=0
+while contador < nomelen:
+    print(nome[contador])
+    contador += 1
---
+++ aula/aula15/aula15.py
@@ -0,0 +1,9 @@
+'''''
+
+função range (start =0, stop, step=1 ) por padrão
+'''''
+
+#para 'n' conte até 10
+#for n in range(10):
+#    print(n)
+
